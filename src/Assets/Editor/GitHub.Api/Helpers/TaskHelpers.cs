84254b111845891368a594f99399eb8be81a3245
Andreia Gaita <shana@spoiledcat.net>
2019-06-20 14:53:47 +0200

    Remove 2.0 profile, move everything under one folder that Unity can compile directly
    Let VS clean up the solution file
    
    Upgrade to .net 4.7.1, move tests out of src dir
    
    Make things compile
    
    Add sfw

---
+++ src/Assets/Editor/GitHub.Api/Helpers/TaskHelpers.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+
+namespace GitHub.Unity
+{
+    static class TaskHelpers
+    {
+        public static Task<T> GetCompletedTask<T>(T result)
+        {
+            return Task.FromResult(result);
+        }
+
+        public static Task<T> ToTask<T>(this Exception exception)
+        {
+            TaskCompletionSource<T> completionSource = new TaskCompletionSource<T>();
+            completionSource.TrySetException(exception);
+            return completionSource.Task;
+        }
+    }
+
+    [Serializable]
+    public class NotReadyException : Exception
+    {
+        public NotReadyException() : base()
+        { }
+        public NotReadyException(string message) : base(message)
+        { }
+        public NotReadyException(string message, Exception innerException) : base(message, innerException)
+        { }
+        protected NotReadyException(SerializationInfo info, StreamingContext context) : base(info, context)
+        { }
+    }
+}
