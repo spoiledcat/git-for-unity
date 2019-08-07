2c61dffd9c8bd97c3e606cfda70f6ca9b1da8d51
Andreia Gaita <shana@spoiledcat.net>
2019-08-07 17:27:15 +0200

    ooops wrong copy

--- src/com.unity.git.api/Api/Primitives/TheVersion.cs
+++ src/com.unity.git.api/Api/Primitives/TheVersion.cs
@@ -1,50 +1,39 @@
+using Unity.VersionControl.Git;
 using System;
 using System.Text.RegularExpressions;
 
 namespace Unity.VersionControl.Git
 {
+    [Serializable]
     public struct TheVersion : IComparable<TheVersion>
     {
-        private const string versionRegex = @"^(?<major>\d+)(\.?(?<minor>\d+))?(\.?(?<patch>\d+))?(\.?(?<build>\d+))?(\.?(?<special>.+))?";
+        private const string versionRegex = @"^(?<major>\d+)(\.?(?<minor>[^.]+))?(\.?(?<patch>[^.]+))?(\.?(?<build>.+))?";
         private const int PART_COUNT = 4;
-        private static TheVersion empty;
-        public static TheVersion Default
-        {
-            get
-            {
-                if (empty.Version == null)
-                    empty = default(TheVersion).Initialize(null);
-                return empty;
-            }
-        }
+        public static TheVersion Default { get; } = default(TheVersion).Initialize(null);
 
-        private int major;
-        public int Major { get { Initialize(Version); return major; } }
-        private int minor;
-        public int Minor { get { Initialize(Version); return minor; } }
-        public bool HasMinor { get; private set; }
-        private int patch;
-        public int Patch { get { Initialize(Version); return patch; } }
-        public bool HasPatch { get; private set; }
-        private int build;
-        public int Build { get { Initialize(Version); return build; } }
-        public bool HasBuild { get; private set; }
-        private string special;
-        public string Special { get { Initialize(Version); return special; } }
-        private bool isAlpha;
-        public bool IsAlpha { get { Initialize(Version); return isAlpha; } }
-        private bool isBeta;
-        public bool IsBeta { get { Initialize(Version); return isBeta; } }
-        private bool isUnstable;
-        public bool IsUnstable { get { Initialize(Version); return isUnstable; } }
-        public bool IsValid { get; private set; }
+        [NotSerialized] private int major;
+        [NotSerialized] public int Major { get { Initialize(Version); return major; } }
+        [NotSerialized] private int minor;
+        [NotSerialized] public int Minor { get { Initialize(Version); return minor; } }
+        [NotSerialized] private int patch;
+        [NotSerialized] public int Patch { get { Initialize(Version); return patch; } }
+        [NotSerialized] private int build;
+        [NotSerialized] public int Build { get { Initialize(Version); return build; } }
+        [NotSerialized] private string special;
+        [NotSerialized] public string Special { get { Initialize(Version); return special; } }
+        [NotSerialized] private bool isAlpha;
+        [NotSerialized] public bool IsAlpha { get { Initialize(Version); return isAlpha; } }
+        [NotSerialized] private bool isBeta;
+        [NotSerialized] public bool IsBeta { get { Initialize(Version); return isBeta; } }
+        [NotSerialized] private bool isUnstable;
+        [NotSerialized] public bool IsUnstable { get { Initialize(Version); return isUnstable; } }
 
-        private int[] intParts;
-        private string[] stringParts;
-        private int currentPart;
-        private string version;
-
-        public string Version { get { return version ?? (version = string.Empty); } set { version = value; } }
+        [NotSerialized] private int[] intParts;
+        [NotSerialized] private string[] stringParts;
+        [NotSerialized] private int parts;
+        [NotSerialized] private bool initialized;
+        [NotSerialized] private string version;
+        public string Version { get { if (version == null) version = String.Empty; return version; } set { version = value; } }
 
         private static readonly Regex regex = new Regex(versionRegex);
 
@@ -53,92 +42,12 @@ namespace Unity.VersionControl.Git
             return default(TheVersion).Initialize(version);
         }
 
-        public static bool TryParse(string version, out TheVersion parsed)
-        {
-            parsed = Parse(version);
-            return parsed.IsValid;
-        }
-
-        public TheVersion BumpMajor()
-        {
-            return ResetFromMinor(string.Format("{0}", Major + 1));
-        }
-
-        public TheVersion BumpMinor()
-        {
-            return ResetFromPatch(string.Format("{0}.{1}", Major, Minor + 1));
-        }
-
-        public TheVersion BumpPatch()
-        {
-            return ResetFromBuild(string.Format("{0}.{1}.{2}", Major, Minor, Patch + 1));
-        }
-
-        public TheVersion BumpBuild()
-        {
-            return ResetFromSpecial(string.Format("{0}.{1}.{2}.{3}", Major, Minor, Patch, Build + 1));
-        }
-
-        public TheVersion BumpLastPart()
-        {
-            if (HasBuild) return BumpBuild();
-            if (HasPatch) return BumpPatch();
-            if (HasMinor) return BumpMinor();
-            return BumpMajor();
-        }
-
-        public TheVersion SetMajor(int newValue)
-        {
-            return ResetFromMinor(string.Format("{0}", newValue));
-        }
-
-        public TheVersion SetMinor(int newValue)
-        {
-            return ResetFromPatch(string.Format("{0}.{1}", Major, newValue));
-        }
-
-        public TheVersion SetPatch(int newValue)
-        {
-            return ResetFromBuild(string.Format("{0}.{1}.{2}", Major, Minor, newValue));
-        }
-
-        public TheVersion SetBuild(int newValue)
-        {
-            return ResetFromSpecial(string.Format("{0}.{1}.{2}.{3}", Major, Minor, Patch, newValue));
-        }
-
-        private TheVersion ResetFromMinor(string ret)
-        {
-            if (HasMinor) ret += ".0";
-            return ResetFromPatch(ret);
-        }
-
-        private TheVersion ResetFromPatch(string ret)
-        {
-            if (HasPatch) ret += ".0";
-            return ResetFromBuild(ret);
-        }
-
-        private TheVersion ResetFromBuild(string ret)
-        {
-            if (HasBuild) ret += ".0";
-            return ResetFromSpecial(ret);
-        }
-
-        private TheVersion ResetFromSpecial(string ret)
-        {
-            if (IsUnstable) ret += Special;
-            return Parse(ret);
-        }
-
         private TheVersion Initialize(string theVersion)
         {
-            if (IsValid)
+            if (initialized)
                 return this;
 
-            Version = String.Empty;
-            if (theVersion != null)
-                Version = theVersion.Trim();
+            this.Version = theVersion?.Trim() ?? String.Empty;
 
             isAlpha = false;
             isBeta = false;
@@ -147,71 +56,84 @@ namespace Unity.VersionControl.Git
             patch = 0;
             build = 0;
             special = null;
-            currentPart = 0;
+            parts = 0;
 
             intParts = new int[PART_COUNT];
             stringParts = new string[PART_COUNT];
             for (var i = 0; i < PART_COUNT; i++)
                 stringParts[i] = intParts[i].ToString();
 
-            if (string.IsNullOrEmpty(theVersion))
+            if (String.IsNullOrEmpty(theVersion))
                 return this;
 
             var match = regex.Match(theVersion);
             if (!match.Success)
             {
+                LogHelper.Error(new ArgumentException("Invalid version: " + theVersion, "theVersion"));
                 return this;
             }
 
             major = int.Parse(match.Groups["major"].Value);
-            intParts[currentPart] = major;
-            stringParts[currentPart] = major.ToString();
+            intParts[parts] = major;
+            stringParts[parts] = major.ToString();
+            parts = 1;
 
             var minorMatch = match.Groups["minor"];
             var patchMatch = match.Groups["patch"];
             var buildMatch = match.Groups["build"];
-            var specialMatch = match.Groups["special"];
 
             if (minorMatch.Success)
             {
-                currentPart++;
-                minor = int.Parse(minorMatch.Value);
-                HasMinor = true;
-                intParts[currentPart] = minor;
-                stringParts[currentPart] = minor.ToString();
-
-                if (patchMatch.Success)
+                if (!int.TryParse(minorMatch.Value, out minor))
                 {
-                    currentPart++;
-                    patch = int.Parse(patchMatch.Value);
-                    HasPatch = true;
-                    intParts[currentPart] = patch;
-                    stringParts[currentPart] = patch.ToString();
+                    special = minorMatch.Value.TrimEnd();
+                    stringParts[parts] = special ?? "0";
+                }
+                else
+                {
+                    intParts[parts] = minor;
+                    stringParts[parts] = minor.ToString();
+                    parts++;
 
-                    if (buildMatch.Success)
+                    if (patchMatch.Success)
                     {
-                        currentPart++;
-                        build = int.Parse(buildMatch.Value);
-                        HasBuild = true;
-                        intParts[currentPart] = build;
-                        stringParts[currentPart] = build.ToString();
+                        if (!int.TryParse(patchMatch.Value, out patch))
+                        {
+                            special = patchMatch.Value.TrimEnd();
+                            stringParts[parts] = special ?? "0";
+                        }
+                        else
+                        {
+                            intParts[parts] = patch;
+                            stringParts[parts] = patch.ToString();
+                            parts++;
+
+                            if (buildMatch.Success)
+                            {
+                                if (!int.TryParse(buildMatch.Value, out build))
+                                {
+                                    special = buildMatch.Value.TrimEnd();
+                                    stringParts[parts] = special ?? "0";
+                                }
+                                else
+                                {
+                                    intParts[parts] = build;
+                                    stringParts[parts] = build.ToString();
+                                    parts++;
+                                }
+                            }
+                        }
                     }
                 }
-            }
-
-            if (specialMatch.Success)
-            {
-                special = specialMatch.Value;
-                stringParts[currentPart] = stringParts[currentPart] + special;
             }
 
             isUnstable = special != null;
             if (isUnstable)
             {
-                isAlpha = special.IndexOf("alpha", StringComparison.Ordinal) >= 0;
-                isBeta = special.IndexOf("beta", StringComparison.Ordinal) >= 0;
+                isAlpha = special.IndexOf("alpha") >= 0;
+                isBeta = special.IndexOf("beta") >= 0;
             }
-            IsValid = true;
+            initialized = true;
             return this;
         }
 
@@ -231,7 +153,7 @@ namespace Unity.VersionControl.Git
 
         public override int GetHashCode()
         {
-            var hash = 17;
+            int hash = 17;
             hash = hash * 23 + Major.GetHashCode();
             hash = hash * 23 + Minor.GetHashCode();
             hash = hash * 23 + Patch.GetHashCode();
@@ -252,7 +174,7 @@ namespace Unity.VersionControl.Git
             return this == other;
         }
 
-        public static bool operator ==(TheVersion lhs, TheVersion rhs)
+        public static bool operator==(TheVersion lhs, TheVersion rhs)
         {
             if (lhs.Version == rhs.Version)
                 return true;
@@ -264,21 +186,21 @@ namespace Unity.VersionControl.Git
                     (lhs.Special == rhs.Special);
         }
 
-        public static bool operator !=(TheVersion lhs, TheVersion rhs)
+        public static bool operator!=(TheVersion lhs, TheVersion rhs)
         {
             return !(lhs == rhs);
         }
 
-        public static bool operator >(TheVersion lhs, TheVersion rhs)
+        public static bool operator>(TheVersion lhs, TheVersion rhs)
         {
             if (lhs.Version == rhs.Version)
                 return false;
-            if (!lhs.IsValid)
+            if (!lhs.initialized)
                 return false;
-            if (!rhs.IsValid)
+            if (!rhs.initialized)
                 return true;
 
-            for (var i = 0; i < lhs.currentPart && i < rhs.currentPart; i++)
+            for (var i = 0; i < lhs.parts && i < rhs.parts; i++)
             {
                 if (lhs.intParts[i] != rhs.intParts[i])
                     return lhs.intParts[i] > rhs.intParts[i];
@@ -294,27 +216,27 @@ namespace Unity.VersionControl.Git
             return false;
         }
 
-        public static bool operator <(TheVersion lhs, TheVersion rhs)
+        public static bool operator<(TheVersion lhs, TheVersion rhs)
         {
             return !(lhs > rhs);
         }
 
-        public static bool operator >=(TheVersion lhs, TheVersion rhs)
+        public static bool operator>=(TheVersion lhs, TheVersion rhs)
         {
             return lhs > rhs || lhs == rhs;
         }
 
-        public static bool operator <=(TheVersion lhs, TheVersion rhs)
+        public static bool operator<=(TheVersion lhs, TheVersion rhs)
         {
             return lhs < rhs || lhs == rhs;
         }
 
         private static int CompareVersionStrings(string lhs, string rhs)
         {
-            int lhsNonDigitPos = 0;
+            int lhsNonDigitPos;
             var lhsNumber = GetNumberFromVersionString(lhs, out lhsNonDigitPos);
 
-            int rhsNonDigitPos = 0;
+            int rhsNonDigitPos;
             var rhsNumber = GetNumberFromVersionString(rhs, out rhsNonDigitPos);
 
             if (lhsNumber != rhsNumber)
@@ -329,13 +251,13 @@ namespace Unity.VersionControl.Git
                 return 1;
             if (rhsNonDigitPos < 0)
                 return -1;
-            return string.Compare(lhs.Substring(lhsNonDigitPos), rhs.Substring(rhsNonDigitPos), StringComparison.Ordinal);
+            return lhs.Substring(lhsNonDigitPos).CompareTo(rhs.Substring(rhsNonDigitPos));
         }
 
         private static int GetNumberFromVersionString(string lhs, out int nonDigitPos)
         {
             nonDigitPos = IndexOfFirstNonDigit(lhs);
-            int number = -1;
+            var number = -1;
             if (nonDigitPos > -1)
             {
                 int.TryParse(lhs.Substring(0, nonDigitPos), out number);
