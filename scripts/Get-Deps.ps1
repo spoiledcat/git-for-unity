27893c45d2c2273493e1c6c23b744fa5572fd6db
Andreia Gaita <shana@spoiledcat.net>
2019-08-15 16:04:53 +0200

    Fix build

--- scripts/Get-Deps.ps1
+++ scripts/Get-Deps.ps1
@@ -16,7 +16,7 @@ try {
     $destfile = Join-Path $destdir 'deps.zip'
     Invoke-WebRequest -usebasicparsing "https://ghfvs-installer.github.com/dependencies/deps.zip" -OutFile $destfile
     Push-Location $destdir
-    Run-Command -Fatal { &'7z' -y -bb3 x 'deps.zip' }
+    Invoke-Command -Fatal { &'7z' -y -bb3 x 'deps.zip' }
 
 } finally {
     Pop-Location
