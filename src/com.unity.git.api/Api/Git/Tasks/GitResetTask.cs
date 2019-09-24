b37104b5356c5e6469c6cf1ab8bdc01824c9ff53
Christopher Addison <christophera@unity3d.com>
2019-09-24 09:45:08 -0700

    Improve readability for getting the reset mode as a string argument

--- src/com.unity.git.api/Api/Git/Tasks/GitResetTask.cs
+++ src/com.unity.git.api/Api/Git/Tasks/GitResetTask.cs
@@ -12,16 +12,29 @@ namespace Unity.VersionControl.Git.Tasks
             CancellationToken token, IOutputProcessor<string> processor = null)
             : base(token, processor ?? new SimpleOutputProcessor())
         {
-            var mode = "";
-            mode +=
-                resetMode == GitResetMode.NonSpecified ? "" :
-                    resetMode == GitResetMode.Soft ? "--soft" :
-                        resetMode == GitResetMode.Keep ? "--keep" :
-                            resetMode == GitResetMode.Mixed ? "--mixed" :
-                                "--hard";
+
 
             Name = TaskName;
-            arguments = $"reset {mode} {changeset}";
+            arguments = $"reset {GetModeString(resetMode)} {changeset}";
+        }
+
+        private string GetModeString(GitResetMode resetMode)
+        {
+            switch (resetMode)
+            {
+                case GitResetMode.NonSpecified:
+                    return string.Empty;
+                case GitResetMode.Soft:
+                    return "--soft";
+                case GitResetMode.Keep:
+                    return "--keep";
+                case GitResetMode.Mixed:
+                    return "--mixed";
+                case GitResetMode.Hard:
+                    return "--hard";
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(resetMode), resetMode, null);
+            }
         }
 
         public override string ProcessArguments { get { return arguments; } }
