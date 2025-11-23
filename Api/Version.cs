a109f64cbb5a32a1200bcc0cad206730c7c6c925
SpoiledBot <github@spoiledcat.net>
2025-11-23 14:48:23 +0000

    com.spoiledcat.git v1.0.59+9f9ac7747e 1.0.59+9f9ac7747e

--- Api/Version.cs
+++ Api/Version.cs
@@ -11,8 +11,8 @@
 #pragma warning disable CA2243
 
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.58.64490")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.58+eafbfa26c5")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.59.39583")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.59+9f9ac7747e")]
 #if NETSTANDARD || NETFRAMEWORK || NETCOREAPP
 [System.CodeDom.Compiler.GeneratedCode("Nerdbank.GitVersioning.Tasks","3.4.255.64262")]
 #endif
@@ -21,13 +21,13 @@
 #endif
 internal static partial class ThisAssembly {
     internal const string AssemblyConfiguration = "Release";
-    internal const string AssemblyFileVersion = "1.0.58.64490";
-    internal const string AssemblyInformationalVersion = "1.0.58+eafbfa26c5";
+    internal const string AssemblyFileVersion = "1.0.59.39583";
+    internal const string AssemblyInformationalVersion = "1.0.59+9f9ac7747e";
     internal const string AssemblyName = "SpoiledCat.Git";
     internal const string AssemblyTitle = "SpoiledCat.Git";
     internal const string AssemblyVersion = "1.0.0.0";
-    internal static readonly System.DateTime GitCommitDate = new System.DateTime(638995054550000000L, System.DateTimeKind.Utc);
-    internal const string GitCommitId = "eafbfa26c55ccbc4beb2c02693b4bffe27856e0d";
+    internal static readonly System.DateTime GitCommitDate = new System.DateTime(638995060490000000L, System.DateTimeKind.Utc);
+    internal const string GitCommitId = "9f9ac7747ec0e418fa6aa9f37ae1864c42768d83";
     internal const bool IsPrerelease = false;
     internal const bool IsPublicRelease = true;
     internal const string RootNamespace = "Api";
--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,5 +2,5 @@
 
 <!-- Do not change the line immediately below this comment, the build system will replace it with the actual version and date. -->
 
-## [1.0.58] - 2025-11-23
+## [1.0.59] - 2025-11-23
 
